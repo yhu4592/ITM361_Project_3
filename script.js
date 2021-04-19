5839572aa7909f729f17f6919c6ada1a9cb3e4b2
yhu4592 <hu.yongkangsg114@gmail.com>
2021-04-18 21:56:54 -0500

    Added google maps page in navigation with script and styling.

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
-    $('.slider').bxSlider({
-        captions: true
-    });
+  $('.slider').bxSlider({
+    captions: true
   });
+});
+
+
+function initMap() {
+  let map;
+  map = new google.maps.Map(document.getElementById("map"), {
+  center: { lat: 41.87884, lng: -87.63596 },
+  zoom: 20,
+  });
+
+  const image =
+    "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
+  const beachMarker = new google.maps.Marker({
+    position: { lat: 41.87884, lng: -87.63596 },
+    map,
+    icon: image,
+  });
+}
+
